50fdf8c9458e412949c9f84281454b478a3032d2
Matheus <matheus.fogiattos@gmail.com>
2020-09-27 11:08:47 -0300

    Creates deafult xcode project

---
+++ MovieDB-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB-MVC
+//
+//  Created by Matheus Fogiatto on 27/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
